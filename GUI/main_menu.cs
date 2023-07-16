dab05532b2de44708a69263250b28d439fb70b4f
Julian Woo <julian.wooville@gmail.com>
2023-07-16 12:52:49 -0400

    main menu skeleton + vscode setup

---
+++ GUI/main_menu.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class main_menu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+}
+
+
+private void _on_start_button_pressed()
+{
+	// Replace with function body.
+}
+
+
+private void _on_options_button_pressed()
+{
+	// Replace with function body.
+}
+
+
+private void _on_quit_button_pressed()
+{
+	// Replace with function body.
+}
