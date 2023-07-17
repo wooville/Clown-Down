f4f9207ed63e1a466d91375c3cb8d0767b6e344f
AlexFediw <50928739+AlexFediw@users.noreply.github.com>
2023-07-16 22:27:54 -0700

    Quick commit

--- GeneratedTileMap.cs
+++ GeneratedTileMap.cs
@@ -6,7 +6,7 @@ public partial class GeneratedTileMap : TileMap
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		GD.Print("Hello");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
